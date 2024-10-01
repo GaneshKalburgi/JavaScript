6dfc9562589c89aaac390546a06b884c92f9289f
GANESH KALBURGI <104773134+GaneshKalburgi@users.noreply.github.com>
2024-10-01 14:45:07 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+/*
+Data Types
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using nodejs,  not browser
+
+console.log(3+3);
+console.log("Ganesh");
+
+let name = "Ganesh"
+let age =18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// booleran =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Ganesh"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
+
