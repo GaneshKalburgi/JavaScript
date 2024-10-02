e3b31917ac5b45fe6413d9f960343837b2a5fbd6
GANESH KALBURGI <104773134+GaneshKalburgi@users.noreply.github.com>
2024-10-02 09:57:25 +0000

    datatype_summaryh

---
+++ datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String , Number , Boolean, null , undefined , Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedInt = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id ===  anotherId);
+
+const bidNumber = 254545668975412n
+
+const heros = ["shaktiman","naagra","doga"];
+let myobj = {
+    name: "ganesh",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("HELLO WORLD");
+}
+
+console.log(typeof myFunction);
+
+
+
+
+
+// Reference (Non Prim0itive)
+
+// Array , Objects, Functions
